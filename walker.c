dba8cbbc2635182ab1b5c50e211e5f8f29ced0d5
Joseph Joe <josephbdjoe@gmail.com>
2015-07-24 15:57:23 -0500

    fixed nanotube table and random walk

--- walker.c
+++ walker.c
@@ -2,24 +2,33 @@
  * walker.c
  */
 #include <gsl_rng.h>
+#include <gsl_cdf.h>
 #include <math.h>
 
 /* 
  * Get New Position on the Nanotube
  */
-int RandomPosBounds(double i[3], double f[3], double *Current,
-		    gsl_rng *rng) {
-  double p1 = gsl_rng_get(rng) / (1.0*gsl_rng_max(rng));
-  double p2 = gsl_rng_get(rng) / (1.0*gsl_rng_max(rng)); 
-  double p3 = gsl_rng_get(rng) / (1.0*gsl_rng_max(rng));
+int Redistribute(double i[3], double f[3], double *Current,
+		 int radius, int fineness, int bins, gsl_rng *rng) {
+  double p1 = gsl_rng_uniform(rng);
+  double p2 = gsl_rng_uniform(rng);
+  double p3 = gsl_rng_uniform(rng);
+  double off = (1.0*radius) / (1.0*bins*fineness);
 
-  Current[0] = i[0] + p1*(f[0] - i[0]);
-  Current[1] = i[1] + p2*(f[1] - i[1]);
-  Current[2] = i[2] + p3*(f[2] - i[2]);
+  double x1, y1, z1;
+  double x2, y2, z2;
+  
+  if (i[0] < f[0]) {x1 = i[0]; x2 = f[0];} else {x1 = f[0]; x2 = i[0];} 
+  if (i[1] < f[1]) {y1 = i[1]; y2 = f[1];} else {y1 = f[1]; y2 = i[1];} 
+  if (i[2] < f[2]) {z1 = i[2]; z2 = f[2];} else {z1 = f[2]; z2 = i[2];} 
+
+  Current[0] = x1 - off + p1*(x2 - x1 + 2.0*off);
+  Current[1] = y1 - off + p1*(y2 - y1 + 2.0*off); 
+  Current[2] = z1 - off + p1*(z2 - z1 + 2.0*off);
   
   return 0;
-  
 }
+
 
 /*
  * Bins are numbered from 0 to n_bins_side^3-1
@@ -29,36 +38,18 @@ int RandomPosBounds(double i[3], double f[3], double *Current,
  * The bins on the boundaries (x=1, y=1, z=1) do contain 
  * their respective upper sides
  */
-int calculate_Bin(double x, double y, double z, int n_bin_side, double side_length) {
-  int xx = x * n_bin_side / side_length; // -> x / (sidelength/n_bin_side)
-  int yy = y * n_bin_side / side_length;
-  int zz = z * n_bin_side / side_length;
+int calculateBin(double x, double y, double z, int bins) {
+  int xx = x * bins; // -> x / (sidelength/n_bin_side)
+  int yy = y * bins;
+  int zz = z * bins;
 
   // If a coordinate is on a boundary, we say it's on the upper side
   // of a bin
-  if (xx == n_bin_side) xx--;
-  if (yy == n_bin_side) yy--;
-  if (zz == n_bin_side) zz--;
+  if (xx == bins) xx--;
+  if (yy == bins) yy--;
+  if (zz == bins) zz--;
 
-  return yy*n_bin_side*n_bin_side + xx + zz*n_bin_side;
-}
-
-/*
- * Returns the azemuthal angle phi, and does not allow a walker to exit
- * from the heated sides.
- */
-double get_phi(double y, gsl_rng *rng, double radius, double side_length) {
-  // The walker is getting close to the boundary y=0, so we bounce it back
-  if (y < radius) {
-    return gsl_rng_get(rng) / (1.0*gsl_rng_max(rng)) * M_PI;
-  }
-  // The walker is getting close to the boundary y=1, so we bounce it back
-  else if (y > (side_length - radius)) {
-    return (gsl_rng_get(rng) / (1.0*gsl_rng_max(rng)) * M_PI) + M_PI;
-  }
-  else {
-    return gsl_rng_get(rng) / (1.0*gsl_rng_max(rng)) * 2 * M_PI;
-  }
+  return yy*bins*bins + xx + zz*bins;
 }
 
 /*
@@ -73,75 +64,72 @@ double get_phi(double y, gsl_rng *rng, double radius, double side_length) {
  *
  * returns 0 if in the matrix, 1 if in a nanotube
  */
-int Random_Walk(double *Current, gsl_rng *rng, double i_N[][3], 
-		double f_N[][3], int *N, double side_length, 
-		int bins, int fineness) {
-
+int RandomWalk(double *Current, gsl_rng *rng, int *N,
+	       double (*iN)[3], double (*fN)[3], int length, int radius,
+	       int fineness, int bins) {
   int in = 0; 
-  double theta; double phi;
-  double radius = 1.0/149.0;
-  // prob. that a walker crosses from the matrix to the nanotube
+  double Dm = 0.02; // diffusivity
+  double sigma = sqrt(2 * Dm);   // standard deviation sqrt(2*D_m*delta_t)
   double fm_cn = 0.5;
-  // prob. that a walker crosses from the nanotube to the matrix
-  double fcn_m = 0.5;
+  double fcn_m = sigma * fineness * bins * 
+    (2.0/(1.0*length)+ 4.0/(1.0*radius)) * fm_cn;
 
-  double x = Current[0]; double y = Current[1]; double z = Current[2];
+  double x = Current[0]; 
+  double y = Current[1];
+  double z = Current[2];
 
-  // if in nanotube, check which nanotube we're in
-  int lookup = N[calculate_Bin(x,y,z,fineness*bins,side_length)];
-  if (lookup != 0) {
-    double prob = gsl_rng_get(rng) / (1.0*gsl_rng_max(rng));
-    if (prob < fcn_m) {
-      // redistribute and return
-      RandomPosBounds(i_N[lookup-1],f_N[lookup-1],Current,rng);
+  /* check if we're inside a nanotube */
+  int lookup = 0;
+  if (N) {lookup = N[calculateBin(x,y,z,fineness*bins)];}
+  if (0 != lookup) {
+    double p = gsl_rng_uniform(rng);
+    if (p < 1.0-fcn_m) {
+      //redistribute in nanotube
+      Redistribute(iN[lookup-1],fN[lookup-1],Current,radius,
+		   fineness,bins,rng);
       return 1;
     }
   }
 
-  // calculate angles  
-  theta = (gsl_rng_get(rng) / (1.0*gsl_rng_max(rng))) * M_PI;
-  phi = get_phi(Current[1], rng, radius, side_length);
-  
-  // potential new positions
-  x += radius*sin(theta)*cos(phi);
-  y += radius*sin(theta)*sin(phi);
-  z += radius*cos(theta);
-  // periodic boundary conditions
-  if (x > side_length) {x -= side_length;}
-  if (x < 0) {x += side_length;}
-  if (z > side_length) {z -= side_length;}
-  if (z < 0) {z += side_length;}
+  // Random Walk according to a Normal Distribution,
+  // potential new positions x y z
+  double s1 = gsl_rng_uniform_pos(rng);
+  x += (sigma / sqrt(3)) * gsl_cdf_ugaussian_Qinv(s1);
 
-  // check if walker enters nanotube
-  lookup = N[calculate_Bin(x,y,z,fineness*bins,side_length)];
-  if (lookup != 0) {
-    double prob = gsl_rng_get(rng) / (1.0*gsl_rng_max(rng));
-    // exits and walker does not get new position if 
-    // probability is lacking
-    if (prob < fm_cn) {
-      return 0;
+  double s2 = gsl_rng_uniform_pos(rng);
+  z += sigma / sqrt(3) * gsl_cdf_ugaussian_Qinv(s2);
+  
+  double s3;
+  while (1) {
+    s3 = gsl_rng_uniform_pos(rng);
+    if ((y + sigma / sqrt(3) * gsl_cdf_ugaussian_Qinv(s3) >= 0) &&
+	(y + sigma / sqrt(3) * gsl_cdf_ugaussian_Qinv(s3) <= 1)) {
+      break;
+    } 
+  }
+  y += sigma / sqrt(3) * gsl_cdf_ugaussian_Qinv(s3);
+
+
+  // periodic boundary conditions
+  while (x > 1.0) { x = x - 1.0;}
+  while (z > 1.0) { z = z - 1.0;}
+  while (x < 0) { x = x + 1.0;}
+  while (z < 0) { z = z + 1.0;}
+
+  /* Check if our new position is located in a nanotube  */
+  if (N) {lookup = N[calculateBin(x,y,z,fineness*bins)];}
+  if (0 != lookup) {
+    double p = gsl_rng_uniform(rng);
+    if (p < 1.0-fm_cn) {
+      return 0; // don't enter nanotube, walker stays put
     }
-    else { in = 1;}
+    else {in = 1;}
   }
 
-  // update x, y, z
-  Current[0] = radius*sin(theta)*cos(phi) + Current[0];
-  Current[1] = radius*sin(theta)*sin(phi) + Current[1];
-  Current[2] = radius*cos(theta) + Current[2];
-  
-  // periodic boundary conditions
-  if (Current[0] > side_length) {
-    Current[0] -= side_length;
-  }
-  if (Current[2] > side_length) {
-    Current[2] -= side_length;
-  }
-  if (Current[0] < 0) {
-    Current[0] += side_length;
-  }
-  if (Current[2] < 0) {
-    Current[2] += side_length;
-  }
-
+  // updates position, enters nanotube
+  Current[0] = x;
+  Current[1] = y;
+  Current[2] = z;
+ 
   return in;  
 }
--- walker.h
+++ walker.h
@@ -1,18 +1,12 @@
 #ifndef WALKER_H_
 #define WALKER_H_
 
-/*
- * Updates the position by performing a random walk
- */
-int Random_Walk(double *Current, gsl_rng *rng, double i_N[][3], double f_N[][3],
-		int *N, double side_length,int bins, int fineness);
 
-/*
- * Returns a value for phi, which depends on the Current y-value,
- * i.e. the boundaries
- */
-double get_phi(double y, gsl_rng *rng, double radius, double side_length);
+int RandomWalk(double *Current, gsl_rng *rng, int *N, 
+	       double (*iN)[3], double (*fN)[3], int length, int radius,
+	       int fineness, int bins);
 
+int calculateBin(double x, double y, double z, int bins);
 
 
 #endif
